7de78f6e73638c5ecb1166035f8219a4d1f221a3
shuto kikai <yysskikai04@icloud.com>
2023-08-04 16:56:13 +0900

    init

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
   end
 end
